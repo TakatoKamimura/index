6e5fd32058c7edb05573d352aa1b01d4194a465f
TakatoKamimura <takato718@outlook.com>
2022-07-07 12:26:46 +0900

    不要なファイルの削除

--- style.js
+++
@@ -1,5 +0,0 @@
-// var modified = new Date(document.lastModified);
-// var year = modified.getFullYear();
-// var month = modified.getMonth() + 1;
-// var date = modified.getDate();
-// document.write("最終更新:" + year + "年" + manth + "月" + date + "日");
